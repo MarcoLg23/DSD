6b82ff3cc0e077829ab2d73eddd4c6e5d4b0f0f5
MarcoLg23 <marcolg23@gmail.com>
2022-04-13 14:48:49 -0500

    clase20

--- CLASE20/WebServer.java
+++ CLASE20/WebServer.java
@@ -21,12 +21,7 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  SOFTWARE.
  */
-
- //LIBRERIAS PARA CONSTRUIR SERVIDOR HTTP EN JAVA
-
-import java.lang.Math;
-import java.util.Random;
-
+//importacion de las librerias necesarias para crear el servidor
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
@@ -36,89 +31,96 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.math.BigInteger;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
+import java.util.*;
 import java.util.concurrent.Executors;
 
 public class WebServer {
-    //CADENAS PARA LAS RESPUESTAS DEL SERVIDOR
+    
+    //declaración de las cadenas que se usan como END POINTS
     private static final String TASK_ENDPOINT = "/task";
     private static final String STATUS_ENDPOINT = "/status";
-    private static final String SEARCH_ENDPOINT = "/search";
-
-    //VARIABLES PARA LA CREACION DEL SERVIDOR
+    private static final String SEARCHIPN_ENDPOINT="/searchipn";
+    //variables privadas para crear el webServer
     private final int port;
     private HttpServer server;
 
     public static void main(String[] args) {
+      // por default el webServer trabaja en el puerto 8080
         int serverPort = 8080;
+        // cuando se pasa el como argumento en la consola se usa el puerto definido por el usario
         if (args.length == 1) {
-            serverPort = Integer.parseInt(args[0]); //SI SE RECIBE UN ENTERO EN EL PRIMER ARGUMENTO, ESE SERA EL PUERTO
+            serverPort = Integer.parseInt(args[0]);
         }
-
-        WebServer webServer = new WebServer(serverPort); //CONFIGURACION DEL SERVIDOR
-        webServer.startServer(); //INICIALIZACION DEL SERVIDOR
-
+        //se crea una instancia de la clase para despues inicializarlo
+        WebServer webServer = new WebServer(serverPort);
+        webServer.startServer();
+        //cuando el servidor arranca se imprime en consola el siguiente mensaje
         System.out.println("Servidor escuchando en el puerto " + serverPort);
     }
-
+    //inicializa la configuracion del servidor
     public WebServer(int port) {
-        this.port = port; //INICIALIZACION VARIABLE PORT
+        this.port = port;
     }
-
     public void startServer() {
         try {
-            this.server = HttpServer.create(new InetSocketAddress(port), 0); //EL SEGUNDO ARGUMENTO ES EL NUMERO DE ELEMENTOS EN COLA, EN 0 ES DECISION DEL SISTEMA
+            // crea una intancia de tcp con el puerto y la ip que se designan
+            // tambien se pone en 0 para la lista de clientes pendientes
+            this.server = HttpServer.create(new InetSocketAddress(port), 0);
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
-
-        HttpContext statusContext = server.createContext(STATUS_ENDPOINT); //CREACION DE OBJETO HTTPCONTEXT SIN HTTPHANDLER PERO CON LA RUTA ASIGNADA
+        //se crean los contextos de HTTP haciendo uso de los ENS POINTS y de sus rutas relativas
+        HttpContext statusContext = server.createContext(STATUS_ENDPOINT);
         HttpContext taskContext = server.createContext(TASK_ENDPOINT);
-        HttpContext searchContext = server.createContext(SEARCH_ENDPOINT);
+        HttpContext searchipnContext = server.createContext(SEARCHIPN_ENDPOINT);
 
-        statusContext.setHandler(this::handleStatusCheckRequest); //VINCULACIÓN DEL OBJETO CON SU METODO
+        //metodos que se vehinculan s los END POINTS de tal manera que cuando se tenga una 
+        //peticion se ejecunte el HANDLE del CONTEXT 
+        statusContext.setHandler(this::handleStatusCheckRequest);
         taskContext.setHandler(this::handleTaskRequest);
-        searchContext.setHandler(this::handleSearchRequest);
-
+        searchipnContext.setHandler(this::handleSearchIPNRequest);
+        //Se crea el THREAD de hilos usando POOL de tamaño 8 y se inicializa
         server.setExecutor(Executors.newFixedThreadPool(8));
-        server.start(); //EJECUCION DEL SERVIDOR EN SEGUNDO PLANO
+        server.start();
     }
-
+    //es el metodo que ejecuta las acciones del END POINT TASK
     private void handleTaskRequest(HttpExchange exchange) throws IOException {
-        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) { //ANALIZA EL TIPO DE REQUEST PARA SABER SI ES TIPO POST
-            exchange.close(); //SI NO LO ES SE CIERRA
+        //se recupera el metodo para asegurarse de que es metodo post en otro caso terminara 
+        //la ejecucion del metodo
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
             return;
         }
-
+        // se recuperan todos los headers para recuprar el X-TEST y si su valor es true se genera la respuesta "123"
         Headers headers = exchange.getRequestHeaders();
-        if (headers.containsKey("X-Test") && headers.get("X-Test").get(0).equalsIgnoreCase("true")) { //VERIFICACION DE HEADER XTEST EN VALOR TRUE
+        if (headers.containsKey("X-Test") && headers.get("X-Test").get(0).equalsIgnoreCase("true")) {
             String dummyResponse = "123\n";
             sendResponse(dummyResponse.getBytes(), exchange);
             return;
         }
-
+        // si entre los headers se encutnra el header X-DEBUG y ademas su valor es true entoncers la variable isDebugMode es establece en true
         boolean isDebugMode = false;
-        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) { //VERIFICACION DE HEADER XDEBUG EN LAVOR TRUE
+        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) {
             isDebugMode = true;
         }
-
-        //RECOLECCION DEL TIEMPO QUE TOMO EL PROCESAMIENTO DE DEPURACION
+        //las siguietes lineas son informacion de depuracion
         long startTime = System.nanoTime();
-
-        byte[] requestBytes = exchange.getRequestBody().readAllBytes(); //RECUPERA EL CUERPO DEL MENSAJE
-        byte[] responseBytes = calculateResponse(requestBytes); //SE ALMACENA EN BYTES
-
+        //recupera la informacion del request especificamente del body 
+        byte[] requestBytes = exchange.getRequestBody().readAllBytes();
+        // se calcula la multiplicacion con los datos tomandolos como int
+        byte[] responseBytes = calculateResponse(requestBytes);
+        //se calgula el tiempo final 
         long finishTime = System.nanoTime();
-
-        if (isDebugMode) { //VERIFICA SI FUE SOLICITADO EL DEBUG
+        //se agrega la info de depuracion en un header de respuesta
+        if (isDebugMode) {
             String debugMessage = String.format("La operación tomó %d nanosegundos", finishTime - startTime);
-            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage)); //SI HAY MODO DEBUG MANDA EL MENSAJE CON LA OPERACION DEL TIEMPO
+            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage));
         }
-
+        //se envia la respuesta 
         sendResponse(responseBytes, exchange);
     }
-
+    //calculate response
     private byte[] calculateResponse(byte[] requestBytes) {
         String bodyString = new String(requestBytes);
         String[] stringNumbers = bodyString.split(",");
@@ -133,17 +135,94 @@ public class WebServer {
         return String.format("El resultado de la multiplicación es %s\n", result).getBytes();
     }
 
+
     private void handleStatusCheckRequest(HttpExchange exchange) throws IOException {
-        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) { //VERIFICA SI LA PETICION ES TIPO CHECK
+      //se asegura de que el metodo es get 
+        if (!exchange.getRequestMethod().equalsIgnoreCase("get")) {
             exchange.close();
             return;
         }
-
-        String responseMessage = "El servidor está vivo\n"; //RESPONDE CON EL MENSAJE
+        //se añade el mensaje de respuesta y se envia la respuesta
+        String responseMessage = "El servidor está vivo\n";
         sendResponse(responseMessage.getBytes(), exchange);
     }
 
-    //CREACION DE LA RESPUESTA Y MANEJO DEL EXCHANGE
+    private void handleSearchIPNRequest(HttpExchange exchange) throws IOException {
+        //se recupera el metodo para asegurarse de que es metodo post en otro caso terminara 
+        //la ejecucion del metodo
+        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) {
+            exchange.close();
+            return;
+        }
+        // se recuperan todos los headers para recuprar el X-TEST y si su valor es true se genera la respuesta "123"
+        Headers headers = exchange.getRequestHeaders();
+        // si entre los headers se encutnra el header X-DEBUG y ademas su valor es true entoncers la variable isDebugMode es establece en true
+        boolean isDebugMode = false;
+        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) {
+            isDebugMode = true;
+        }
+        //las siguietes lineas son informacion de depuracion
+        long startTime = System.nanoTime();
+        //recupera la informacion del request especificamente del body 
+        byte[] requestBytes = exchange.getRequestBody().readAllBytes();
+        // se calcula la multiplicacion con los datos tomandolos como int
+        byte[] responseBytes = calcularCadenota(requestBytes);
+        //se calgula el tiempo final 
+        long finishTime = System.nanoTime();
+        //se agrega la info de depuracion en un header de respuesta
+        if (isDebugMode) {
+            String debugMessage = String.format("La operación tomó %d nanosegundos", finishTime - startTime);
+            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage));
+        }
+        //se envia la respuesta 
+        sendResponse(responseBytes, exchange);
+    }
+    private byte[] calcularCadenota(byte[] request){
+      String aux= new String(request);
+      String tokens[]= aux.split(",");
+      int n = Integer.parseInt(tokens[0]);
+        int min = 65;
+    int max = 90;
+    StringBuilder cadenota= new StringBuilder();
+    int contador =0;
+
+    for (int i =0; i <n; i++) {
+//     Random random = new Random();
+     cadenota.append(String.valueOf((char)((new Random()).nextInt(max-min+1)+min)));
+     cadenota.append(String.valueOf((char)((new Random()).nextInt(max-min+1)+min)));
+     cadenota.append(String.valueOf((char)((new Random()).nextInt(max-min+1)+min)));
+     cadenota.append(" ");
+     //char r2 = (char)((new Random()).nextInt(max-min+1)+min);
+     //char r3 = (char)((new Random()).nextInt(max-min+1)+min);
+     //char espacio =' ';
+
+    }
+    ArrayList<Integer> Posiciones = new  ArrayList<Integer>();
+    int ocurrencias=0;
+    int endIndex=0;
+    boolean flag=true;
+    //encontrar la palabra IPN en la cadenota
+      while (flag){
+        int i=cadenota.indexOf(tokens[1],endIndex);
+        if (i!=-1){
+          Posiciones.add(i);
+          ocurrencias++;
+          endIndex=i+1;
+        }else flag=false;
+      }
+    System.out.println(Posiciones);
+    System.out.println(ocurrencias);
+//    String a = new String(cadenota, StandardCharsets.UTF_8);
+//    System.out.println(cadenota);
+            
+    return String.format("El numero de veces que se encuentra %s es: %d\n",tokens[1],ocurrencias).getBytes();
+//    String a = new String(cadenota, StandardCharsets.UTF_8);
+  //  System.out.println(a);
+        
+  }
+
+    
+    //funcion de respuesta en al que se añade el status code, la longitud de la respuesta, los headers y el cuerpo de la respuesta
     private void sendResponse(byte[] responseBytes, HttpExchange exchange) throws IOException {
         exchange.sendResponseHeaders(200, responseBytes.length);
         OutputStream outputStream = exchange.getResponseBody();
@@ -151,68 +230,5 @@ public class WebServer {
         outputStream.flush();
         outputStream.close();
         exchange.close();
-    }
-
-//-------------------METODO SEARCH IPN
-
-    private void handleSearchRequest(HttpExchange exchange) throws IOException {
-        if (!exchange.getRequestMethod().equalsIgnoreCase("post")) { //ANALIZA EL TIPO DE REQUEST PARA SABER SI ES TIPO POST
-            exchange.close(); //SI NO LO ES SE CIERRA
-            return;
-        }
-
-        Headers headers = exchange.getRequestHeaders();
-        boolean isDebugMode = false;
-        if (headers.containsKey("X-Debug") && headers.get("X-Debug").get(0).equalsIgnoreCase("true")) { //VERIFICACION DE HEADER XDEBUG EN LAVOR TRUE
-            isDebugMode = true;
-        }
-
-        //RECOLECCION DEL TIEMPO QUE TOMO EL PROCESAMIENTO DE DEPURACION
-        long startTime = System.nanoTime();
-
-        byte[] requestBytes = exchange.getRequestBody().readAllBytes(); //RECUPERA EL CUERPO DEL MENSAJE
-        byte[] responseBytes = calculateString(requestBytes); //SE ALMACENA EN BYTES
-
-        long finishTime = System.nanoTime();
-
-        if (isDebugMode) { //VERIFICA SI FUE SOLICITADO EL DEBUG
-            String debugMessage = String.format("La operación tomó %d nanosegundos", finishTime - startTime);
-            exchange.getResponseHeaders().put("X-Debug-Info", Arrays.asList(debugMessage)); //SI HAY MODO DEBUG MANDA EL MENSAJE CON LA OPERACION DEL TIEMPO
-        }
-
-        sendResponse(responseBytes, exchange);
-    }
-
-    private byte[] calculateString(byte[] requestBytes) {
-        String bodyString = new String(requestBytes);
-        String[] strings = bodyString.split(",");
-
-        int firstUppercaseIndex = (int)'A';
-        int n = new Integer(strings[0]);
-        String[] cadena = new String[n];
-        Random lr = new Random();
-
-        String tmp = "";
-        for(int i = 0; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
-                int letterIndex = lr.nextInt(26);
-                char letra = (char) (firstUppercaseIndex + letterIndex);
-                tmp = tmp + letra;
-            }
-            cadena[i] = tmp;
-            
-        }
-        
-        String delimiter = "";
-        String result = String.join(delimiter, cadena);
-        
-        
-        int count = 0;
-        for(int i = 0; i < result.length(); i++) {
-            if(result.charAt(i) == (strings[1]) && result.charAt(i+1) == (strings[2]) && result.charAt(i+2) == (strings[3])) {
-                count++;
-            }
-        }
-        return String.format("La cadena se encontro %d veces\n", count).getBytes();
     }
 }
